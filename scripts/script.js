a41a3c6233eabcf3d39080019f6f0623b2a9541d
Kitkat <mr7kitkat@gmail.com>
2022-06-02 13:02:05 +0530

    make app full functioning and responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,40 @@
+
+// Actual implimentation
+const form = document.querySelector("form.createNewbook");
+
+const newBookCreateBtn = document.querySelector("#addNewBook");
+const bookShelf = document.querySelector(".booklist");
+
+
+// FORM CONTROLS
+const titleInput = form.querySelector("#btitle");
+const descriptionInput = form.querySelector("#bdescription");
+const pagesInput = form.querySelector("#pagesCount");
+const readStatusInput = form.querySelector("#readStatus");
+const formClose = form.querySelector("img.form-close");
+
+
+
+// this array will work as database
 const LIBRARY = [
-    `
-        <div class="book">
-        <h3>the ramayana</h3>
-        <div class="details flex">
-            <p class="pages">1000 pages</p>
-
-            <div class="actions" data-value="0">
-                <img src="./images/icons/pencil.png" class="edit" alt="">
-                <img src="./images/icons/close.png" class="delete" alt="">
-            </div>
-        </div>
-
-        <p class="description">A tale of Lord ram's life</p>
-        </div>
-    `,
-    `
-        <div class="book">
-        <h3>Iron man</h3>
-        <div class="details flex">
-            <p class="pages">500 pages</p>
-
-            <div class="actions" data-value="1">
-                <img src="./images/icons/pencil.png" class="edit" alt="">
-                <img src="./images/icons/close.png" class="delete" alt="">
-            </div>
-        </div>
-
-        <p class="description">A superhero story</p>
-        </div>
-    `
+    {
+        description: "THE BOOK OF AMAN",
+        pages: "100",
+        readStatus: "Not read yet!",
+        title: "AMAN",
+    },
+    {
+        description: "THE BOOK OF RITESH",
+        pages: "200",
+        readStatus: "Not read yet!",
+        title: "RITESH",
+    },
+    {
+        description: "THE BOOK OF SHASHI",
+        pages: "400",
+        readStatus: "Not read yet!",
+        title: "SHASHI",
+    },
 ];
 
 function BOOK(title, pages, description, readStatus, db) {
@@ -42,40 +48,74 @@ function BOOK(title, pages, description, readStatus, db) {
 
 BOOK.prototype.addBook = function() {
     const {title, pages, description, readStatus, db} = this;
+    db.push({title, pages, description, readStatus});
+}
 
-    const tag = `
-    <div class="book">
-        <h3>${title}</h3>
-        <div class="details flex">
-            <p class="pages">${pages} pages</p>
+function render(parentElem, arr) {
+    const tags = arr.map(item => {
+    const {title, pages, description, readStatus} = item;
+    return `
+        <div class="book">
+            <h3>${title}</h3>
+            <div class="details flex">
+                <p class="pages">${pages} pages</p>
 
-            <div class="actions" data-value="${db.length}">
-                <img src="./images/icons/pencil.png" class="edit" alt="">
-                <img src="./images/icons/close.png" class="delete" alt="">
+                <div class="actions" data-value="${arr.indexOf(item)}">
+                    <img src="./images/icons/pencil.png" class="edit" alt="">
+                    <img src="./images/icons/close.png" class="delete" alt="">
+                </div>
             </div>
+
+            <p class="description">${description}</p>
         </div>
-
-        <p class="description">${description}</p>
-    </div>
-    `
-    db.push(tag);
-}
-
-BOOK.prototype.render = function(parentElem) {
-    const tags = this.db.map(item => item).join("");
+    `}).join("");
     parentElem.innerHTML = tags;
+    console.log();
+    deleteBook( arr, parentElem);
+    editBook(arr, parentElem);
+}
+
+
+function deleteBook(arr, parentElem) {
+    const taglist = parentElem.querySelectorAll("img.delete");
+    taglist.forEach(tag => {
+        tag.addEventListener("click", (e) => {
+            const idx = Number(e.target.parentElement.dataset.value);
+            arr.splice(idx, 1);
+            render(parentElem, arr);
+        })
+    })
 }
 
 
 
+function editBook(arr, parentElem) {
+    const taglist = parentElem.querySelectorAll("img.edit");
+    let obj = null;
+    
+    taglist.forEach(tag => {
+        tag.addEventListener("click", (e) => {
+            const idx = Number(e.target.parentElement.dataset.value);
+            obj = arr[idx];
+            form.classList.add("display");
 
-// Actual implimentation
-const form = document.querySelector("form.createNewbook");
-const newBookCreateBtn = document.querySelector("#addNewBook");
-const bookShelf = document.querySelector(".booklist");
-// FORM CONTROLS
+            titleInput.value = obj.title;
+            descriptionInput.value = obj.description;
+            pagesInput.value = obj.pages;
+            readStatusInput.value = obj.value;
 
-const formClose = form.querySelector("img.form-close");
+            arr.splice(idx, 1);
+        })
+    })
+}
+
+
+
+// On Load 
+window.addEventListener("load", () => {
+    render(bookShelf, LIBRARY);
+});
+
 
 // Buttons functions
 newBookCreateBtn.addEventListener("click", () => {
@@ -93,14 +133,14 @@ function closeForm() {
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const title = form.querySelector("#btitle").value;
-    const description = form.querySelector("#bdescription").value;
-    const pages = form.querySelector("#pagesCount").value;
-    const readStatus = form.querySelector("#readStatus").value;
-    const saveBtn = form.querySelector("button");
+    const title = titleInput.value;
+    const description = descriptionInput.value;
+    const pages = pagesInput.value;
+    const readStatus = readStatusInput.value;
 
     const newbook = new BOOK(title, pages, description, readStatus, LIBRARY);
-    newbook.render(bookShelf);
+    render(bookShelf, LIBRARY);
+
     closeForm();
 })
 
